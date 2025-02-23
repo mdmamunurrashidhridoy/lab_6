7e5f30b20a2c5b0e6d8a00d4c1a75907fff3915b
mdmamunurrashidhridoy <rashid15-5273@diu.edu.bd>
2025-02-23 12:23:07 +0600

    Working_with_drawer

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,61 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+          drawer: Drawer(
+            backgroundColor: Colors.amber[100],
+              child: ListView(
+            children: [
+              SizedBox(
+                height: 150,
+                child: DrawerHeader(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.tiktok),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Icon(Icons.close),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text('Home'),
+                leading: Icon(Icons.home),
+                onTap: () {},
+                focusColor: Colors.amber[200]!,
+              ),
+              ListTile(
+                title: Text('Inbox'),
+                leading: Icon(Icons.inbox),
+                onTap: () {},
+                trailing: Text("40"),
+              ),
+              ListTile(
+                title: Text('email'),
+                leading: Icon(Icons.email),
+                onTap: () {},
+                trailing: Text("120"),
+              ),
+              ListTile(
+                title: Text('Read Mail'),
+                leading: Icon(Icons.mark_email_read),
+                onTap: () {},
+              ),
+            ],
+          )),
           appBar: AppBar(
             title: Text(
               'Advance UI Design',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 15,
+                fontWeight: FontWeight.bold,
               ),
             ),
             centerTitle: true,
-            backgroundColor: Colors.blueAccent,
+            backgroundColor: Colors.amber,
           ),
           body: Column(
             children: [
