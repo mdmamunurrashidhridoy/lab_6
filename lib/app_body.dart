db53e54c92278222c7fc9fe66560b80f6568cd5f
mdmamunurrashidhridoy <rashid15-5273@diu.edu.bd>
2025-02-23 12:56:55 +0600

    page_change_with_Bottom_nav_bar

--- lib/app_body.dart
+++ lib/app_body.dart
@@ -8,38 +8,59 @@ class Appbody extends StatefulWidget {
 }
 
 class _AppbodyState extends State<Appbody> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = [
+    Text('Home'),
+    Text('Search'),
+    Text('Add'),
+    Text('Favorite'),
+    Text('Profile'),
+  ];
+
+  void onTapMethod(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(
+        child: pages[currentIndex],
+      ),
       bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-              backgroundColor: Colors.amber,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Search',
-              backgroundColor: Colors.amber,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add),
-              label: 'Add',
-              backgroundColor: Colors.amber,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.favorite),
-              label: 'Favorite',
-              backgroundColor: Colors.amber,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle),
-              label: 'Profile',
-              backgroundColor: Colors.amber,
-            ),
-          ],
-        ),
+        onTap: onTapMethod,
+        currentIndex: currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+            backgroundColor: Colors.amber,
+          ),
+        ],
+      ),
     );
   }
 }
