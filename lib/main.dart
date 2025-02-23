503657deb949aed88aada4fa3147632597ff2519
mdmamunurrashidhridoy <rashid15-5273@diu.edu.bd>
2025-02-23 12:35:25 +0600

    Bottom_navigation

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          drawer: Drawer(
-            backgroundColor: Colors.amber[100],
-              child: ListView(
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.amber,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+              backgroundColor: Colors.amber,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Add',
+              backgroundColor: Colors.amber,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorite',
+              backgroundColor: Colors.amber,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'Profile',
+              backgroundColor: Colors.amber,
+            ),
+          ],
+        ),
+        drawer: Drawer(
+          backgroundColor: Colors.amber[100],
+          child: ListView(
             children: [
               SizedBox(
                 height: 150,
@@ -54,29 +83,45 @@ class MyApp extends StatelessWidget {
                 onTap: () {},
               ),
             ],
-          )),
-          appBar: AppBar(
-            title: Text(
-              'Advance UI Design',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            centerTitle: true,
-            backgroundColor: Colors.amber,
           ),
-          body: Column(
+        ),
+        appBar: AppBar(
+          title: Text(
+            'Advance UI Design',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
             children: [
+              CachedNetworkImage(
+                imageUrl:
+                    "https://images.unsplash.com/photo-1728774283246-b0e6eea31e34?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
               CachedNetworkImage(
                 imageUrl:
                     "https://images.unsplash.com/photo-1727233431563-15019f08ef1e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                 placeholder: (context, url) => CircularProgressIndicator(),
                 errorWidget: (context, url, error) => Icon(Icons.error),
               ),
+              CachedNetworkImage(
+                imageUrl:
+                    "https://images.unsplash.com/photo-1727233429981-f069ff6fad6b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
